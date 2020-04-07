06bff02b6919c039e22b711f1c347ebd88377dcf
Edward <wardzxzxc@gmail.com>
2020-04-08 01:04:18 +0800

    Change Login
    - removed backend auth and sign up
    - created login endpoint which just takes in address and store
    - changed User model to just contain public address

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,9 @@ const user = require('../controller/user');
 //2. Import Express Router
 const router = express.Router();
 
-//Sign Up
-router.post('/signup', user.signUp);
 //Login
 router.post('/login', user.login);
 //Get User (currently only by address)
 router.get('/', user.getUser);
-//Metamask auth
-router.post('/auth', user.auth);
 
 module.exports = router;
