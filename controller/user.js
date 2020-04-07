a0a3dd29c9272181604c2273505dc0b3cd32351f
Edward Wang <wardzxzxc@gmail.com>
2020-04-08 01:26:22 +0800

    Change Login (#9)
    - removed backend auth and sign up
    - created login endpoint which just takes in address and store
    - changed User model to just contain public address

--- controller/user.js
+++ controller/user.js
@@ -1,62 +1,25 @@
 const User = require('../models/Users.js');
-const firebase = require('../firebase/firebase.js');
-const jwt = require('jsonwebtoken');
-const config = require('../config');
-const ethUtil = require('ethereumjs-util');
-const sigUtil = require('eth-sig-util');
 
-module.exports.signUp = async (req, res, next) => {
+module.exports.login = async(req, res, next) => {
     const publicAddress = req.body.publicAddress;
-    const role = req.body.role;
+    const query = {
+        publicAddress: publicAddress
+    };
     try {
-        await User.create({
-            publicAddress: publicAddress,
-            role: role
-        });
-        return res.status(201).json({
-            message: "Sign Up successful"
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: 'An error occurred',
-            error: error.message
-        })
-    }
-};
-
-module.exports.login = async (req, res, next) => {
-    try {
-        const firebaseRes = await firebase.auth().signInWithEmailAndPassword(
-            req.body.email, req.body.password
-        );
-        const firebaseId = firebaseRes.user.uid;
-        const user = await User.findOne({"email": req.body.email}).exec();
-        if (user) {
-            const payload = {
-                firebaseId,
-                role: user.role,
-                firstName: user.firstName,
-                lastName: user.lastName,
-                email: user.email
-            };
-            const token = jwt.sign(payload, config.jwtSecret, {
-                expiresIn: "12H"
+        const user = await User.findOne(query).exec();
+        if(user == null) {
+            //Create new user if not found
+            await User.create({
+                publicAddress: publicAddress,
             });
-            return res.status(200).json({
-                message: "Login successful",
-                token: token
-            })
-        } else {
-            return res.status(404).json({
-                message: 'An error occurred',
-                error: 'User cannot be found'
-            })
         }
-    } catch (error) {
-        return res.status(500).json({
-            message: 'An error occurred',
-            error: error.message
-        })
+        return res.status(200).json({
+            message: "Login successful",
+        });
+    } catch(error) {
+        return res.status(400).json({
+            message: "Login unsuccessful",
+        });
     }
 };
 
@@ -82,47 +45,48 @@ module.exports.getUser = async(req, res, next) => {
     }
 };
 
-module.exports.auth = async(req, res, next) => {
-    const publicAddress = req.body.publicAddress;
-    const signature = req.body.signature;
-    try {
-        const user = await User.findOne({publicAddress: publicAddress});
-        const msg = `I am signing my one-time nonce: ${user.nonce}`;
-
-        const msgBufferHex = ethUtil.bufferToHex(Buffer.from(msg, 'utf8'));
-        const address = sigUtil.recoverPersonalSignature({
-            data: msgBufferHex,
-            sig: signature
-        });
-        if (address.toLowerCase() === publicAddress.toLowerCase()) {
-            //Change to new nonce
-            user.nonce = Math.floor(Math.random() * 10000);
-            user.save();
-            const payload = {
-                id: user.id,
-                publicAddress
-            };
-            const token = jwt.sign(payload, config.jwtSecret, {
-                expiresIn: "12H"
-            });
-            return res.status(200).json({
-                message: "Login successful",
-                token: token,
-            });
-        } else {
-            return res
-                .status(401)
-                .json({
-                    message: "Login unsuccessful",
-                    error: 'Signature verification failed' });
-        }
-    } catch(err) {
-        return res.status(500).json({
-            message: 'An error occurred',
-            error: err
-        })
-    }
-};
+// module.exports.auth = async(req, res, next) => {
+//     const publicAddress = req.body.publicAddress;
+//     const signature = req.body.signature;
+//     try {
+//         const user = await User.findOne({publicAddress: publicAddress});
+//         const msg = `I am signing my one-time nonce: ${user.nonce}`;
+//
+//         const msgBufferHex = ethUtil.bufferToHex(Buffer.from(msg, 'utf8'));
+//         const address = sigUtil.recoverPersonalSignature({
+//             data: msgBufferHex,
+//             sig: signature
+//         });
+//         if (address.toLowerCase() === publicAddress.toLowerCase()) {
+//             //Change to new nonce
+//             user.nonce = Math.floor(Math.random() * 10000);
+//             user.save();
+//             const payload = {
+//                 id: user.id,
+//                 publicAddress
+//             };
+//             const token = jwt.sign(payload, config.jwtSecret, {
+//                 expiresIn: "12H"
+//             });
+//             return res.status(200).json({
+//                 message: "Login successful",
+//                 token: token,
+//                 role: user.role
+//             });
+//         } else {
+//             return res
+//                 .status(401)
+//                 .json({
+//                     message: "Login unsuccessful",
+//                     error: 'Signature verification failed' });
+//         }
+//     } catch(err) {
+//         return res.status(500).json({
+//             message: 'An error occurred',
+//             error: err
+//         })
+//     }
+// };
 
 
 
--- models/Users.js
+++ models/Users.js
@@ -3,13 +3,6 @@ const mongoose = require('mongoose');
 //Schema is a method in the mongoose library which takes 1 JSON argument
 const Users = new mongoose.Schema({
     publicAddress: {type: String, unique: true},
-    nonce: {type: Number, default: Math.floor(Math.random() * 1000000)},
-    role: {
-        type: String,
-        enum : ['admin', 'event organiser', 'customer'],
-        default: 'customer',
-        required: true
-    },
 });
 
 module.exports = mongoose.model('Users', Users);
